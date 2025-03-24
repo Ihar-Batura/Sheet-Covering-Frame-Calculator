6bf226aba6fcfdae2b9176ea7f8cef4ad4c706de
Ihar-Batura <A17331582@gmail.com>
2025-03-24 20:41:13 +0300

    feat: add code for main page

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
 import './sass/style.scss';
+import createHeader from './components/header';
+import createMain from './components/main';
+import createFooter from './components/footer';
+
+const body: HTMLElement = document.body;
+
+const header: HTMLElement = createHeader();
+const main: HTMLElement = createMain();
+const footer: HTMLElement = createFooter();
+
+body.append(header, main, footer);
