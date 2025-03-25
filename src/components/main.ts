c24efd652c4494bddfe1fc2bd300ede4ed31b41d
Ihar-Batura <A17331582@gmail.com>
2025-03-25 17:56:16 +0300

    feat: add create main page

--- src/components/main.ts
+++ src/components/main.ts
@@ -1,13 +1,19 @@
 import createElement from '../create/create_element';
+import createInputContainer from './inputContainer';
 
 function createMain(): HTMLElement {
   const main: HTMLElement = createElement({ tag: 'main', classes: ['main'] });
 
-  const mainWrapper: HTMLElement = createElement({
+  const wrapper: HTMLElement = createElement({
     tag: 'div',
     classes: ['wrapper', 'main-wrapper'],
     parent: main,
   });
+
+  const inputContainer: HTMLElement = createInputContainer();
+  const resultContainer: HTMLElement = createElement({ tag: 'div', classes: ['result-container'] });
+
+  wrapper.append(inputContainer, resultContainer);
 
   return main;
 }
